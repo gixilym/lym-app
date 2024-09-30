236f9e4efbdf305dfc6aa3bf066afee6a651c187
gixi <gixi.tsx@gmail.com>
2024-09-30 17:16:10 -0300

    pdf con info

---
+++ src/components/PDFBtn.jsx
@@ -0,0 +1,44 @@
+function PDFBtn() {
+  return (
+    <section
+      id="pdf"
+      className="flex flex-col items-center justify-center pb-20 pt-4 bg-purple-950/15"
+    >
+      <p className="text-lg md:text-xl text-gray-200 mb-4 text-center">
+        Este documento PDF contiene información <br /> detallada acerca de
+        nuestros servicios
+      </p>
+      <a
+        href="/lym.pdf"
+        download
+        className="
+        border-2 border-purple-600
+          flex items-center justify-center
+          bg-purple-700 hover:bg-purple-800
+          text-white font-bold py-3 px-6 rounded-lg
+          transition duration-300 ease-in-out
+          transform hover:scale-105 focus:outline-none
+          focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50
+        "
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 mr-2 transition-transform duration-300"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+          />
+        </svg>
+        Descargar PDF
+      </a>
+    </section>
+  );
+}
+
+export default PDFBtn;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import AboutUs from "../components/AboutUs";
 import Contact from "../components/Contact";
 import FAQ from "../components/FAQ";
 import Main from "../components/Main";
+import PDFBtn from "../components/PDFBtn";
 import Services from "../components/Services";
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
       <Contact />
       <AboutUs />
       <FAQ />
+      <PDFBtn />
     </>
   );
 }
